2c4c7c7f3a4e096fe61f58f0c3872468d8a2bfd8
Moni Singh <121425974+Moni-Singh@users.noreply.github.com>
2023-01-09 16:04:10 +0530

    Initial

---
+++ app/src/main/java/com/example/chatbotrobot/data/Message.kt
@@ -0,0 +1,4 @@
+package com.example.chatbotrobot.data
+
+
+data class Message(val message: String, val id: String, val time: String)
---
+++ app/src/main/java/com/example/chatbotrobot/utils/BotResponse.kt
@@ -0,0 +1,107 @@
+package com.example.chatbotrobot.utils
+
+import com.example.chatbotrobot.utils.Constants.OPEN_GOOGLE
+import com.example.chatbotrobot.utils.Constants.OPEN_SEARCH
+import java.sql.Date
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+
+object BotResponse {
+
+    fun basicResponses(_message: String): String {
+
+        val random = (0..2).random()
+        val message =_message.toLowerCase()
+
+        return when {
+
+            //Flips a coin
+            message.contains("flip") && message.contains("coin") -> {
+                val r = (0..1).random()
+                val result = if (r == 0) "heads" else "tails"
+
+                "I flipped a coin and it landed on $result"
+            }
+
+            //Math calculations
+            message.contains("solve") -> {
+                val equation: String? = message.substringAfterLast("solve")
+                return try {
+                    val answer = SolveMath.solveMath(equation ?: "0")
+                    "$answer"
+
+                } catch (e: Exception) {
+                    "Sorry, I can't solve that."
+                }
+            }
+
+            //Hello
+            message.contains("hello") -> {
+                when (random) {
+                    0 -> "Hello there!"
+                    1 -> "How can i help you"
+                    2 -> "are you felling hungry"
+                    else -> "error" }
+            }
+
+            //How are you?
+            message.contains("how are you") -> {
+                when (random) {
+                    0 -> "I'm doing fine, thanks!"
+                    1 -> "I'm hungry..."
+                    2 -> "Pretty good! How about you?"
+                    else -> "error"
+                }
+            }
+
+            //
+            message.contains("I need help") -> {
+                when(random){
+                    0 ->"Where do you want to travel"
+                    1 ->"International or Domestic"
+                    2 ->"oops please say"
+                    else ->"error"
+                }
+            }
+
+            message.contains("International") -> {
+                    when(random) {
+                        0 -> " Please wait we are checking your data"
+                        1 -> " wait a second let e check your data"
+                        2 ->"Check the data"
+                        else -> "error"
+
+                }
+            }
+
+            //What time is it?
+            message.contains("time") && message.contains("?")-> {
+                val timeStamp = Timestamp(System.currentTimeMillis())
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm")
+                val date = sdf.format(Date(timeStamp.time))
+
+                date.toString()
+            }
+
+            //Open Google
+            message.contains("open") && message.contains("google")-> {
+                OPEN_GOOGLE
+            }
+
+            //Search on the internet
+            message.contains("search")-> {
+                OPEN_SEARCH
+            }
+
+            //When the programme doesn't understand...
+            else -> {
+                when (random) {
+                    0 -> "I don't understand..."
+                    1 -> "Try asking me something different"
+                    2 -> "Can you try to say something else"
+                    else -> "error"
+                }
+            }
+        }
+    }
+}
